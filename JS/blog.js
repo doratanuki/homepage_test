6336317b777b586384d88901d8e50a8de7bb4fb4
doratanuki <128441152+doratanuki@users.noreply.github.com>
2024-02-19 01:40:06 +0900

    Add files via upload

--- JS/blog.js
+++ JS/blog.js
@@ -8,13 +8,13 @@ function test(){
     var sec3height = $("#section3").outerHeight();
 
     //section1切るY座標
-    var v1 = ScrollBottom - (sec1height + titleheight + 100)
+    var v1 = ScrollBottom - (sec1height + titleheight + 120)
     $(".sec1bg img").css("clip-path", `inset(0px 0px ${v1}px 0px)`);
 
-    var v2 = v1 - sec2height - 75
+    var v2 = v1 - sec2height - 100
     $(".sec2bg img").css("clip-path", `inset(${WindowHeight - v1}px 0px ${v2}px 0px)`);
 
-    var v3 = v2 - sec3height -200
+    var v3 = v2 - sec3height -170
     $(".sec3bg img").css("clip-path", `inset(${WindowHeight - v2}px 0px ${v3}px 0px)`);
 }
 
