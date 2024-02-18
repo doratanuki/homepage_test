d42abccd95178a981f8a9ca2f8a0ccda0c02f1b6
doratanuki <128441152+doratanuki@users.noreply.github.com>
2024-02-19 02:24:07 +0900

    Add files via upload

--- JS/blog.js
+++ JS/blog.js
@@ -2,20 +2,19 @@ function test(){
     var ScrollTop = $(window).scrollTop();
     var WindowHeight = $(window).height();
     var ScrollBottom = ScrollTop + WindowHeight
-    var titleheight = $(".title").outerHeight();
-    var sec1height = $("#section1").outerHeight();
-    var sec2height = $("#section2").outerHeight();
-    var sec3height = $("#section3").outerHeight();
+
+    //座標取得
+    var sec2top = $("#section2").offset();
+    var sec3top = $("#section3").offset();
 
     //section1切るY座標
-    var v1 = ScrollBottom - (sec1height + titleheight + 120)
+    var v1 = ScrollBottom - sec2top.top + 50
     $(".sec1bg img").css("clip-path", `inset(0px 0px ${v1}px 0px)`);
-
-    var v2 = v1 - sec2height - 100
+    //section2
+    var v2 = ScrollBottom - sec3top.top + 50
     $(".sec2bg img").css("clip-path", `inset(${WindowHeight - v1}px 0px ${v2}px 0px)`);
-
-    var v3 = v2 - sec3height -170
-    $(".sec3bg img").css("clip-path", `inset(${WindowHeight - v2}px 0px ${v3}px 0px)`);
+    //section3
+    $(".sec3bg img").css("clip-path", `inset(${WindowHeight - v2}px 0px 0px 0px)`);
 }
 
 
