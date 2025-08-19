8473eb8a68b5cac24f5db06b06cc7f9399d9bf1c
doratanuki <128441152+doratanuki@users.noreply.github.com>
2025-08-20 01:33:19 +0900

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,67 @@
+function fadeAnime(){
+    $(".profile").each(function(){
+        var Offset = $(this).offset().top;
+        var ScrollTop = $(window).scrollTop();
+        var WindowHeight = $(window).height();
+        if(Offset - 0 <= ScrollTop + WindowHeight){
+            $(this).addClass('fadeUp');
+        }
+    });
+
+
+    $(".about").each(function(){
+        var Offset = $(this).offset().top;
+        var ScrollTop = $(window).scrollTop();
+        var WindowHeight = $(window).height();
+        if(Offset - 50 <= ScrollTop + WindowHeight){
+            $(this).addClass('fadeUp');
+        }
+    });
+    
+
+    $(".contact").each(function(){
+        var Offset = $(this).offset().top;
+        var ScrollTop = $(window).scrollTop();
+        var WindowHeight = $(window).height();
+        if(Offset - 50 <= ScrollTop + WindowHeight){
+            $(this).addClass('fadeUp');
+        }
+    });
+
+}
+
+
+function fadeScroll(){
+    var ScrollTop = $(window).scrollTop();
+    var WindowHeight = $(window).height();
+    var ScrollBottom = ScrollTop + WindowHeight
+    
+    //画面1
+    var opa1 = 1 - ScrollTop / 450;
+    $(".img-and-name").css("opacity", opa1);
+
+    //PROFILE
+    var opa2 =  ScrollTop / 100 ;
+    $(".profile").css("opacity", opa2);
+
+    //ABOUT
+    var Y_about = $(".about").offset().top;
+    var opa3 =  (ScrollBottom - Y_about) / 100 ;
+    $(".about").css("opacity", opa3);
+
+    //CONTACT
+    var Y_contact = $(".contact").offset().top;
+    var opa4 = (ScrollBottom - Y_contact) / 100;
+    $(".contact").css("opacity", opa4);
+}
+
+$(window).scroll(function(){
+    fadeAnime();
+    fadeScroll();
+});
+
+$(function(){
+    var windowHeight = $(window).height();
+    console.log(windowHeight);
+    $('.img-and-name').css('height',`${windowHeight}px`);
+});
---
+++ JS/slick_common.js
@@ -0,0 +1,17 @@
+$('.slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  fade:true,//切り替えをフェードで行う。初期値はfalse。
+  autoplaySpeed: 1500,//次のスライドに切り替わる待ち時間
+  speed:1800,//スライドの動きのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 1,//スライドを画面にx枚見せる
+  slidesToScroll: 1,//1回のスクロールでx枚の写真を移動して見せる
+  arrows: true,//左右の矢印あり
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: false,//下部ドットナビゲーションの表示
+  pauseOnFocus: false,//フォーカスで一時停止を無効
+  pauseOnHover: false,//マウスホバーで一時停止を無効
+  pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+  swipe: false
+});
