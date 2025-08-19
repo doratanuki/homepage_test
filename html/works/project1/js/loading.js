6ce188f9e6d51673af3a937d2193ef41faa45aa2
doratanuki <128441152+doratanuki@users.noreply.github.com>
2025-08-20 01:31:44 +0900

    Delete html/works directory

--- html/works/project1/js/loading.js
+++
@@ -1,5 +0,0 @@
-//ローディング画面
-$(window).on('load',function(){
-    $("#splash").delay(2500).fadeOut('slow');
-    $("#splash_logo").delay(2500).fadeOut('slow');
-});
--- html/works/project4/openbtn2/openbtn2.js
+++
@@ -1,9 +0,0 @@
-$(".openbtn").click(function () {
-    $(this).toggleClass('active');
-    $(".header-menu").toggleClass('panelactive');
-});
-
-$(".header-menu-item a").click(function () {//ナビゲーションのリンクがクリックされたら
-    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
-    $(".header-menu").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
-});
